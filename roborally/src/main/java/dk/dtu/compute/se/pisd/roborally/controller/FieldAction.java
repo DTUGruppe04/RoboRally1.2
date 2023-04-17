ed9e9cd8cb87ecc37cf354829acd77a75cb00eb2
PH4MTOM <Simonmadsen99@gmail.com>
2023-04-17 08:51:17 +0200

    Added FieldAction.java and CheckPoint.java

---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/FieldAction.java
@@ -0,0 +1,7 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public abstract class FieldAction {
+    public abstract boolean doAction(GameController gameController, Space space);
+}

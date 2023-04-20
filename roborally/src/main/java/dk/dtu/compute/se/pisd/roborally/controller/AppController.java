c79fde4ee31aedc021640ce12474f25986889124
valde12 <s224289@DTU.dk>
2023-04-20 13:25:39 +0200

    made another map

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -104,7 +104,7 @@ public class AppController implements Observer {
             //     here we just create an empty board with the required number of players.
 
 
-            Board board = new Board(PremadeMaps.CORRIDOR_BLITZ.mapArray, "testMap");
+            Board board = new Board(PremadeMaps.SPRINT_CRAMP.mapArray, "testMap");
             gameController = new GameController(board);
             int no = result.get();
             for (int i = 0; i < no; i++) {

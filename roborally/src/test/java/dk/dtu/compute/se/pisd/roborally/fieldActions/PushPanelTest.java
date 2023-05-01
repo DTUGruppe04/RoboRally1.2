6128c18654c0f09705a1d6385dfec1fc3c709ae5
PH4MTOM <Simonmadsen99@gmail.com>
2023-05-01 18:35:20 +0200

    Made a new TestMap. Set spawnpoints in PremadeMaps.java for all maps

--- roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/fieldActions/PushPanelTest.java
+++ roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/fieldActions/PushPanelTest.java
@@ -1,9 +1,23 @@
 package dk.dtu.compute.se.pisd.roborally.fieldActions;
 
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.view.PremadeMaps;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PushPanelTest {
+    private GameController gameController;
+
+    //Creating a 8x8 board for testing of PushPanels
+    void setUp() {
+        //
+        PremadeMaps testMap = PremadeMaps.get("Test Map");
+        Board board = new Board(testMap.mapArray, "Test Map");
+        gameController = new GameController(board);
+
+
+    }
 }

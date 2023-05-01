0bcf752669732a0b7071a5290808de8f1dc0ac9c
PH4MTOM <Simonmadsen99@gmail.com>
2023-05-01 15:11:00 +0200

    New Testing Branch

---
+++ roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/fieldActions/PushPanelTest.java
@@ -0,0 +1,9 @@
+package dk.dtu.compute.se.pisd.roborally.fieldActions;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PushPanelTest {
+}

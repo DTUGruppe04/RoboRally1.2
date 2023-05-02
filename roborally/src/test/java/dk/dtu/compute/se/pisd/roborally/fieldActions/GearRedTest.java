48a41b8bf4cace7ee32f08ce835473e9d95e505d
PH4MTOM <Simonmadsen99@gmail.com>
2023-05-02 16:23:35 +0200

    Added Test to Red and Green gears

---
+++ roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/fieldActions/GearRedTest.java
@@ -0,0 +1,63 @@
+package dk.dtu.compute.se.pisd.roborally.fieldActions;
+
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.*;
+import dk.dtu.compute.se.pisd.roborally.view.PremadeMaps;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+
+@ExtendWith(ApplicationExtension.class)
+public class GearRedTest {
+    private GameController gameController;
+    private Board board;
+    final private List<String> PLAYER_COLORS = Arrays.asList("red", "green", "blue", "orange", "grey", "magenta");
+
+    @BeforeEach
+    void setUp() {
+        //Choosing our test map
+        PremadeMaps testMap = PremadeMaps.get("Test Map");
+        board = new Board(testMap.mapArray, testMap.mapName);
+        gameController = new GameController(board);
+
+        //Creating 2 players and add them to the board
+        for(int i = 0; i < 2; i++) {
+            Player player = new Player(board, PLAYER_COLORS.get(i), "Player " + (i + 1));
+            board.addPlayer(player);
+        }
+        //Spawning the players in the game
+        board.setCurrentPlayer(board.getPlayer(0));
+        gameController.spawnPlayers();
+    }
+
+    @AfterEach
+    void tearDown() {
+        gameController = null;
+        board = null;
+    }
+
+    @Test
+    void redGearRotatePlayerRight() {
+        Player player1 = board.getPlayer(0);
+        Space space = board.getSpace(11, 5);
+
+        player1.setSpace(space);
+        player1.setHeading(Heading.NORTH);
+
+        assertEquals(space.getPlayer(), player1);
+        assertEquals(Heading.NORTH, player1.getHeading());
+
+        gameController.executeBoardElements();
+
+        assertEquals(player1.getHeading(), Heading.EAST);
+
+    }
+
+}
